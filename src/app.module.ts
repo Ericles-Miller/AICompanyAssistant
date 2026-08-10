8906a14d4a692fdea2a7dcc6be7e6423d63215d8
Éricles Miller <ericlesmiller15@gmail.com>
2026-08-10 14:57:40 -0300

    feat: create entities of project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './database/database.provider';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [],
   providers: [],
 })
