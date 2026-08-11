ab29bc1d1a8314884416617e9e7b791bee8d13c7
Éricles Miller <ericlesmiller15@gmail.com>
2026-08-11 15:28:48 -0300

    feat: create a new user method

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { dataSourceOptions } from './database/database.provider';
 import { ChatModule } from './chat/chat.module';
 import { DocumentsModule } from './documents/documents.module';
 import { RetrievalModule } from './retrieval/retrieval.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), DocumentsModule, RetrievalModule, ChatModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), DocumentsModule, RetrievalModule, ChatModule, UsersModule],
   controllers: [],
   providers: [],
 })
